29a4ec4948d6ddc903b0e6cc1899150cf757cfc0
Daniel Griesshaber <dangrie158@gmail.com>
2016-06-14 11:37:36 +0200

    added rcswitch route in example to show off openhab binding

--- examples/restfulExample/index.js
+++ examples/restfulExample/index.js
@@ -3,10 +3,13 @@
 const PORT = process.env.PORT || 80;
 const SCLK_PIN = 7;
 const DATA_PIN = 0;
+const SEND_BINARY = '/sbin/send';
+const RC_NETWORK_CODE = '11101';
 
 const express = require('express');
 const hx711 = require('hx711');
 const bodyparser = require('body-parser');
+const childProcess = require('child_process');
 
 const app = express();
 const sensor = new hx711(SCLK_PIN, DATA_PIN);
@@ -57,6 +60,34 @@ app.post('/tare', (req, res) => {
                 scale: sensor.getScale(),
                 offset: sensor.getOffset()
         });
+});
+
+app.post('/switch/:id/:action', (req, res) => {
+	var id = req.params.id;
+	var action = req.params.action;
+
+	var actionParam = action == 'on' ? '1' : '0';
+	var idParam = '';
+
+	switch(id){
+	case 'A':
+		idParam = '1';
+	break;
+	case 'B':
+                idParam = '2';
+        break;
+	case 'C':
+                idParam = '3';
+        break;
+	case 'D':
+                idParam = '4';
+        break;
+	}
+	
+	var process = childProcess.spawn(SEND_BINARY, [RC_NETWORK_CODE, idParam, actionParam], {uid: 0});
+	process.on('close', (code, signal) => {
+		res.send(signal);
+	});
 });
 
 app.listen(PORT, () => {
